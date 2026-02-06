3ddfc4957ffab577ce7448ba6b5a8f0852aa1496
Saleh Adib Hasnat <salehadib00@gmail.com>
2026-02-06 23:10:40 +0600

    a

--- server.js
+++ server.js
@@ -17,6 +17,14 @@ const allowedOrigins = [
   "http://localhost:5173",
 ];
 
+app.options(
+  "*",
+  cors({
+    origin: allowedOrigins,
+    credentials: true,
+  }),
+);
+
 app.use(
   cors({
     origin: allowedOrigins,
